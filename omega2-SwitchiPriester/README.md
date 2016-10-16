7c5dadcefd1fd43a9d5039c61372ddf78d069786
MP <jasojohns@gmail.com>
2016-10-16 14:45:22 -0400

    Reinitializing with all files from project

---
+++ omega2-SwitchiPriester/README.md
@@ -0,0 +1,10 @@
+# switchiPriester6
+The 6th iteration of the Switchi Priester project
+
+This repository will hold the code needed for the 6th version of my SwitchiPriester project being developed for a Onion Omega 2 
+microprocessor. The SwitchiPriester is a midi controller to change settings on my guitar rig. The project is starting out with some 
+Python code for reading the pedal configuration data.
+
+Project Timeline:
+10/13/2016  - Initial Repository Created and code uploaded
+11/2016     - Reciept of Onion Omega2 boards and other hardware from kickstarter 
---
+++ omega2-SwitchiPriester/config/Scenes.py
@@ -0,0 +1,29 @@
+'''
+Created on Aug 29, 2016
+
+@author: mp
+https://docs.python.org/2/library/xml.etree.elementtree.html
+'''
+
+import xml.etree.ElementTree as ET
+
+class Scene:
+    'Common base class for all employees'
+    name = ''
+
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        print "Scene Name: ", self.name
+        
+scenesFile = ET.parse('../scenes.xml')
+scenesRoot = scenesFile.getroot()
+scenes = []
+
+for scene in scenesRoot:
+    scenes.append(Scene(scene.get('name')))
+    
+for scene in scenes:
+    scene.display()
+    
